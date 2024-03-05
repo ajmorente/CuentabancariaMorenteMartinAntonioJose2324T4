c75907f073d65c24a9d7a53f3c906b6fa43495f7
antmo <antmo@DESKTOP-CJJNM1K>
2024-03-05 01:41:51 +0100

    Mi proyecto MorenteMartinAntonioJose2324 comentado con Javadoc

--- src/cuenta/CuentaMorenteMartinAntonioJose2324T4.java
+++ src/cuenta/CuentaMorenteMartinAntonioJose2324T4.java
@@ -1,8 +1,18 @@
 package cuenta;
+/**
+ * Clase CuentaMorenteMartinAntonioJose2324T4 que representa una cuenta bancaria.
+ * @author antmo
+ */
 
 
 public class CuentaMorenteMartinAntonioJose2324T4 {
-
+/**
+ * Atributos de la clase
+ * private String nombre; // Nombre del titular de la cuenta.
+ * private String cuenta; // Número de la cuenta.
+ * private double saldo; // Saldo actual de la cuenta.
+ * private double tipoInteres; // Tipo de interés de la cuenta.
+ */
     /**
      * @return the nombre
      */
@@ -39,7 +49,7 @@ public class CuentaMorenteMartinAntonioJose2324T4 {
     }
 
     /**
-     * @param saldo the saldo to set
+     * @param saldo the saldo to set    
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
@@ -64,12 +74,20 @@ public class CuentaMorenteMartinAntonioJose2324T4 {
     private String cuenta;
     private double saldo;
     private double tipoInteres;
-
+    /**
+    * Constructor por defecto de la clase.
+    */
    
     public CuentaMorenteMartinAntonioJose2324T4 ()
     {
     }
-   
+   /**
+    * Constructor con parámetros de la clase.
+    * @param nom Nombre del titular de la cuenta.
+    * @param cue Número de la cuenta.
+    * @param sal Saldo inicial de la cuenta.
+    * @param tipo Tipo de interés de la cuenta.
+    */
    
     public CuentaMorenteMartinAntonioJose2324T4 (String nom, String cue, double sal, double tipo)
     {
@@ -78,26 +96,39 @@ public class CuentaMorenteMartinAntonioJose2324T4 {
         saldo=sal;
         tipoInteres=tipo;
     }
-   
+ /**
+  * Método para asignar un nombre al titular de la cuenta
+  * @param nom Nomnre del titular.
+  */
     
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-   
+   /**
+    * Método para obtener el nombre del titular de la cuenta.
+    * @return Nombre del titular
+    */
    
     public String obtenerNombre()
     {
         return getNombre();
     }
-
+/**
+ * Método para obtener el estado actual de la cuenta.
+ * @return Saldo actual de la cuenta.
+ */
     
      public double estado ()
     {
         return getSaldo();
     }
-
-   
+/**
+ * Método para ingresar una cantidad en la cuenta.
+ * @param cantidad Cantidad a ingresar.
+ * @param concepto Concepto del ingreso.
+ * @throws Exception Si la cantidad es negativa.
+ */
     public void ingresar(double cantidad, String concepto) throws Exception
     {
         if (cantidad<0)
@@ -105,7 +136,11 @@ public class CuentaMorenteMartinAntonioJose2324T4 {
         setSaldo(getSaldo() + cantidad);
     }
 
-    
+/**
+ * Método para retirar una cantidad de la cuenta.
+ * @param cantidad Cantidad a retirar.
+ * @throws Exception Si la cantidad es negativa o si no hay suficiente saldo.
+ */    
     public void retirar (double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -114,8 +149,10 @@ public class CuentaMorenteMartinAntonioJose2324T4 {
             throw new Exception ("No hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
-
-    
+/**
+ * Método para obtener el número de la cuenta.
+ * @return Número de la cuenta.
+ */ 
     public String obtenerCuenta ()
     {
         return getCuenta();
--- src/cuenta/Main.java
+++ src/cuenta/Main.java
@@ -1,21 +1,30 @@
 package cuenta;
-
-
+/**
+ *Clase principal que contiene el método main y las operaciones de la cuenta.
+ * @author antmo
+ */
 public class Main {
-
-    
+/**
+ * Método principal que se ejecuta al iniciar el programa.
+ * @param args Argumentos de la línea de comandos.
+ */  
     public static void main(String[] args) {
         CuentaMorenteMartinAntonioJose2324T4 miCuentaMorenteMartinAntonioJose2324T4;
         double saldoActual;
         int x;
-        
+        /**
+         * Creación de una nueva cuenta con los datos proporcionados.
+         */
         miCuentaMorenteMartinAntonioJose2324T4 = new CuentaMorenteMartinAntonioJose2324T4("Antonio Sánchez","ES55-3058-2365-8522-1234-5678",2500,0);
         System.out.println("Saldo inicial de la cuenta: "+miCuentaMorenteMartinAntonioJose2324T4.estado()+"€");
         opera_retiraDineroMorenteMartinAntonioJose2324T4(miCuentaMorenteMartinAntonioJose2324T4);
         
         opera_ingresaDineroMorenteMartinAntonioJose2324T4(miCuentaMorenteMartinAntonioJose2324T4);
     }
-
+/**
+ * Método para realizar una operación de ingreso de dinero en la cuenta.
+ * @param miCuentaMorenteMartinAntonioJose2324T4 La cuenta en la que se va a ingresar el dinero.
+ */
     private static void opera_ingresaDineroMorenteMartinAntonioJose2324T4(CuentaMorenteMartinAntonioJose2324T4 miCuentaMorenteMartinAntonioJose2324T4) {
         int x;
         double saldoActual;
@@ -31,7 +40,10 @@ public class Main {
         saldoActual = miCuentaMorenteMartinAntonioJose2324T4.estado();
         System.out.println("El saldo actual es "+ saldoActual );
     }
-
+/**
+ * Método para realizar una operación de retirada de dinero de la cuenta.
+ * @param miCuentaMorenteMartinAntonioJose2324T4 La cuenta de la que se va a retirar el dinero.
+ */
     private static void opera_retiraDineroMorenteMartinAntonioJose2324T4(CuentaMorenteMartinAntonioJose2324T4 miCuentaMorenteMartinAntonioJose2324T4) {
         int x;
         try
